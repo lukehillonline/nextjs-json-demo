6e0de4b1acff01c04a9463fda4c9b6aeae1aef40
Luke Hill <lukehillonline@gmail.com>
2022-01-24 18:55:17 +0000

    setting up test

---
+++ components/Title/Title.jsx
@@ -0,0 +1,5 @@
+import content from "./Title.content.json";
+
+export function Title() {
+  return <h1>{content.title}</h1>;
+}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  trailingSlash: true,
+  productionBrowserSourceMaps: true,
+
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.content.json$/,
+      type: "asset/source",
+    });
+
+    return config;
+  },
+};
